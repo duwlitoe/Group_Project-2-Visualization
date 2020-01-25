0d39b3107b66daaa9c1d7fd990bacbe364d2d1e7
Duwli Toe <Duwli.toe@gmail.com>
2020-01-25 07:52:52 -0500

    updated folder

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def get_data():
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5000 )))
